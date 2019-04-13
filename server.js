dcf22f220d5368800ed128def63dcfc1eaa90145
jasonasav <jasonasav@gmail.com>
2019-04-12 17:02:35 -0700

    proxy is working locally

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/:id', express.static(path.join(__dirname, 'Public')));
+
+// app.use(express.static())
+
+// app.use('/')
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
